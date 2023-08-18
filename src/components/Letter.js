48a35bb911a07e51785837ece681a7ac55d5fe91
Michiel van der Geest <hello@michielvandergeest.com>
2023-08-18 11:41:32 +0200

    Removed  setting from Letter component used in Intro page.

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -2,13 +2,12 @@ import Bolt from '@lightningjs/bolt'
 
 export default Bolt.Component('Letter', {
   template: `
-    <Element x="$x">
+    <Element>
       <Element w="$w" :h.transition="{v: 410+$offset, d: $duration, w: $wait, f: $timingFunction}"}" color="#E6E6E6" />
       <Element w="$w" h="280" :imageSource="$image" :y.transition="{v: 400+$offset, d: $duration, w: $wait, f: $timingFunction}" />
       <Element w="$w" :h.transition="{v: 500-$offset, d: $duration, w: $wait, f: $timingFunction}"}" :y.transition="{v: 660+$offset, d: $duration, w: $wait, f: $timingFunction}" color="#E6E6E6" />
-
     </Element>`,
-  props: ['w', 'x', 'letter', 'direction', 'delay'],
+  props: ['w', 'letter', 'direction', 'delay'],
   computed: {
     image() {
       return `${window.location.protocol}//${window.location.host}/assets/${this.letter}.png`
