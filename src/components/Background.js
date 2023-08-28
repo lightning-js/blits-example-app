e33c967d47cbc589f96fbbde1910db68f8ed470a
Michiel van der Geest <hello@michielvandergeest.com>
2023-08-28 10:47:37 +0200

    Added slight alpha to background.

--- src/components/Background.js
+++ src/components/Background.js
@@ -4,7 +4,7 @@ export default Bolt.Component('Background', {
   template: /*html*/ `
     <Element>
       <Image
-        :src="$src" w="1920" h="1080" colorBottom="black"
+        :src="$src" w="1920" h="1080" colorBottom="#000" alpha="0.8"
       />
     </Element>`,
   props: ['src'],
