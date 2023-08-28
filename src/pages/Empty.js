8f0a9c9c7be75105a23c0e9e03cc91a72bd2483d
Michiel van der Geest <hello@michielvandergeest.com>
2023-08-28 11:08:23 +0200

    Added simple animation to 'empty' page.

--- src/pages/Empty.js
+++ src/pages/Empty.js
@@ -1,7 +1,25 @@
 import Bolt from '@lightningjs/bolt'
 
-export default Bolt.Component('Alpha', {
+export default Bolt.Component('Empty', {
   template: /*html*/ `
-    <Element></Element>
+    <Element x="880" y="500">
+      <Circle size="40" color="#94a3b8" :alpha.transition="{v: $alpha, w: 200}" />
+      <Circle size="40" color="#94a3b8" x="60" :alpha.transition="{v: $alpha, w: 300}" />
+      <Circle size="40" color="#94a3b8" x="120" :alpha.transition="{v: $alpha, w: 400}" />
+    </Element>
   `,
+  state() {
+    return {
+      alpha: 0,
+      count: 1,
+    }
+  },
+  hooks: {
+    render() {
+      this.$setInterval(() => {
+        this.alpha = this.count % 2 ? 0 : 1
+        this.count++
+      }, 800)
+    },
+  },
 })
