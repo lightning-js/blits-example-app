e85704a16aa59800ba9563ef2cd101808f2a3599
Michiel van der Geest <hello@michielvandergeest.com>
2023-08-29 14:38:33 +0200

    Removed log from api call.

--- src/api/providers/fetchPopular.js
+++ src/api/providers/fetchPopular.js
@@ -4,7 +4,6 @@ export default function (type) {
   return api.get(`/${type}/popular`).then((popular) => {
     let filteredItems = popular.results.filter((r) => !r.adult)
     return filteredItems.map((item) => {
-      console.log(item)
       return {
         poster: getImageUrl(item.poster_path || item.profile_path),
         background: getImageUrl(item.backdrop_path, 'w1280'),
