b641fc849cce4341b582936dd6813eedc4c3a2ce
michielvandergeest <michielvandergeest@users.noreply.github.com>
2024-01-03 17:00:07 +0000

    deploy: a83708fbade1d87b61b746145e7707def63ce545

--- assets/Loading-57b53968.js
+++ assets/Loading-57b53968.js
@@ -1,4 +1,4 @@
-import{B as e,S as l}from"./index-216c7cd6.js";import"./settings-b6f7d147.js";const n=e.Component("Loading",{components:{Square:l},template:`
+import{B as e,S as l}from"./index-bcfba889.js";import"./settings-b6f7d147.js";const n=e.Component("Loading",{components:{Square:l},template:`
     <Element>
       <Element x="880" y="500">
         <Circle size="40" color="#94a3b8" :alpha.transition="{value: $alpha, delay: 200}" />
