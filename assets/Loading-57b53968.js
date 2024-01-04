b94168a513c7e0842252cbb6f0b357c167a9ae22
michielvandergeest <michielvandergeest@users.noreply.github.com>
2024-01-04 16:00:41 +0000

    deploy: 31b15177536ab291f1f174705495f9a81c3824fb

--- assets/Loading-57b53968.js
+++
@@ -1,9 +0,0 @@
-import{B as e,S as l}from"./index-bcfba889.js";import"./settings-b6f7d147.js";const n=e.Component("Loading",{components:{Square:l},template:`
-    <Element>
-      <Element x="880" y="500">
-        <Circle size="40" color="#94a3b8" :alpha.transition="{value: $alpha, delay: 200}" />
-        <Circle size="40" color="#94a3b8" x="60" :alpha.transition="{value: $alpha, delay: 300}" />
-        <Circle size="40" color="#94a3b8" x="120" :alpha.transition="{value: $alpha, delay: 400}" />
-      </Element>
-    </Element>
-  `,state(){return{alpha:0}},hooks:{ready(){let a=0;this.$setInterval(()=>{this.alpha=a%2?0:1,a++},800)}}});export{n as default};
