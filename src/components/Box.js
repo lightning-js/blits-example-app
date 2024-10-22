9810471ca75484435d890b473347cfaf9741525c
jfboeve <jeffrey_boeve@comcast.com>
2024-10-22 14:43:39 +0200

    styled key code page and added back press features

--- src/components/Box.js
+++ src/components/Box.js
@@ -19,11 +19,14 @@ import Blits from '@lightningjs/blits'
 
 export default Blits.Component('Box', {
   template: `
-    <Element w="300" h="350" color="">
-      <Element w="300" h="50" color="blue">
-        <Text x="70" y="10" :content="$header" wordwrap="200" />
-      </Element>
-      <Text y="150" x="80" :content="$text" color="black" wordwrap="200" />
+    <Element
+      w="330"
+      h="180"
+      :effects="[$shader('radius', {radius: 20}), $shader('border', {width: 1, color: '#e2e8f0'})]"
+      color="{top: '#667a97', bottom: '#475569'}"
+    >
+      <Text x="25" y="20" :content="$header" w="280" color="black" contain="width" />
+      <Text x="25" y="90" :content="$text" size="52" w="280" contain="width" align="center" />
     </Element>
   `,
   props: ['header', 'text'],
--- src/pages/KeyCodeDemo.js
+++ src/pages/KeyCodeDemo.js
@@ -7,37 +7,64 @@ export default Blits.Application({
   },
   template: `
     <Element w="1920" h="1080">
-      <Element>
-        <Text :content="$pageTitle" x="750" y="100" size="50" w="500" h="200" />
-        <Text :content="$value" x="750" y="200" size="200" w="500" h="200" />
+      <Element y="250">
+        <Text :content="$pageTitle" font="raleway" mount="{x: 0.5}" x="960" size="62" w="800" h="200" contain="width" />
+        <Text :content="$value" mount="{x: 0.5}" x="960" y="100" size="32" w="800" h="200" contain="width" />
       </Element>
-      <Box w="100" size="200" x="300" y="600" header="$header1" :text="$text1" />
-      <Box w="100" size="200" x="700" y="600" header="$header2" :text="$text2" />
-      <Box w="100" size="200" x="1100" y="600" header="$header3" :text="$text3" />
+      <Element x="395">
+        <Box w="100" size="200" y="600" header="$header1" :text="$text1" />
+        <Box w="100" size="200" x="400" y="600" header="$header2" :text="$text2" />
+        <Box w="100" size="200" x="800" y="600" header="$header3" :text="$text3" />
+      </Element>
+      <Text mount="{x: 1}" x="1860" y="1000" :content="$backLabel" :color="$backPressed ? '#fff' : '#667a97'" />
     </Element>
-    `,
+  `,
   state() {
     return {
-      pageTitle: 'KeyCode',
-      value: 'keycode',
-      header1: 'key  ',
-      header2: 'code  ',
-      header3: 'keyCode',
-      text1: 'text1',
-      text2: 'text2',
-      text3: 'text3',
+      backPressed: false,
+      backTimer: null,
+      goBack: false,
+      pageTitle: 'Key Code Finder',
+      value:
+        'Press any key to get the JavaScript keydown event key, code, which and keyCode properties:',
+      header1: 'e.key',
+      header2: 'e.code',
+      header3: 'e.keyCode',
+      text1: '...',
+      text2: '...',
+      text3: '...',
     }
+  },
+  computed: {
+    backLabel() {
+      return this.goBack ? 'Release to return' : 'Hold Back'
+    },
+  },
+  methods: {
+    updateLabels(e) {
+      this.text1 = e.key
+      this.text2 = e.code
+      this.text3 = e.keyCode + ''
+    },
   },
   input: {
     any(e) {
-      this.pageTitle = 'Key Code is'
-      this.value = e.keyCode
-      this.text1 = e.key
-      this.text2 = e.code
-      this.text3 = e.keyCode
+      this.updateLabels(e)
     },
-    back() {
-      this.$router.back()
+    back(e) {
+      this.updateLabels(e)
+      this.backPressed = true
+      this.backTimer = this.$setTimeout(() => {
+        this.goBack = true
+      }, 2000)
+
+      return () => {
+        this.$clearTimeout(this.backTimer)
+        this.backPressed = false
+        if (this.goBack) {
+          this.$router.back()
+        }
+      }
     },
   },
 })
