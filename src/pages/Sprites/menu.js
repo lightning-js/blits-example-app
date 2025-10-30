da5090c9777c87116ffa9b849cbc71c7c4d7be9a
Suresh Kumar Gangumalla <sureshkumar.gangumalla@invecas.com>
2025-10-30 17:19:08 +0530

    Corrected sprite icon dimentions after paddings
    Updated x, y positions and width, height as per image original
    dimentions and paddings
    
    Signed-off-by: Suresh Kumar Gangumalla <sureshkumar.gangumalla@invecas.com>

--- src/pages/Sprites/menu.js
+++ src/pages/Sprites/menu.js
@@ -22,10 +22,10 @@ export default Blits.Component('MenuSprite', {
     <Element>
       <Element src="assets/menusprite.png" x="748" y="120" w="425" h="340" />
       <Element y="680" x="560">
-        <Sprite image="assets/menusprite.png" x="0" w="140" h="140" map="$map" :frame="$icon1" />
-        <Sprite image="assets/menusprite.png" x="220" w="140" h="140" map="$map" frame="icon2" />
-        <Sprite image="assets/menusprite.png" x="440" w="140" h="140" map="$map" frame="icon3" />
-        <Sprite image="assets/menusprite.png" x="660" w="140" h="140" map="$map" frame="icon4_unfocus" />
+        <Sprite image="assets/menusprite.png" x="0" w="160" h="160" map="$map" :frame="$icon1" />
+        <Sprite image="assets/menusprite.png" x="220" w="160" h="160" map="$map" frame="icon2" />
+        <Sprite image="assets/menusprite.png" x="440" w="160" h="160" map="$map" frame="icon3" />
+        <Sprite image="assets/menusprite.png" x="660" w="160" h="160" map="$map" frame="icon4_unfocus" />
       </Element>
     </Element>
   `,
@@ -38,11 +38,11 @@ export default Blits.Component('MenuSprite', {
           h: 160,
         },
         frames: {
-          icon1_focus: { x: 0, y: 0 },
-          icon1_unfocus: { x: 170, y: 340 },
-          icon2: { x: 170, y: 0 },
-          icon3: { x: 350, y: 0 },
-          icon4_unfocus: { x: 680, y: 170 },
+          icon1_focus: { x: 5, y: 5 },
+          icon1_unfocus: { x: 175, y: 345 },
+          icon2: { x: 175, y: 5 },
+          icon3: { x: 345, y: 5 },
+          icon4_unfocus: { x: 685, y: 175 },
         },
       },
     }
